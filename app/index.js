c24ac9d069bb0570bc4f040ca4aafd9cb31d4d23
Islessian0409 <mr.edward.isles@gmail.com>
2018-07-06 19:41:46 +0800

    Parsing HTTP Methods

--- app/index.js
+++ app/index.js
@@ -6,8 +6,10 @@ const server = http.createServer((req, res) => {
 	let parsedUrl = url.parse(req.url, true);
 	let path = parsedUrl.pathname;
 	let trimmedPath = path.replace(/^\/+|\+$/g, '');
+	let method = req.method.toLowerCase();
 	res.end("Hello World\n");
 	console.log("Request received from: " + trimmedPath);
+	console.log("HTTP Method: " + method);
 });
 
 server.listen(port);
