ec8fe2549a7f8e9722bc988a738713e394bfb8a7
Islessian0409 <mr.edward.isles@gmail.com>
2018-07-07 08:14:13 +0800

    Parsing Payloads

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
 const port = 8000;
 
 const server = http.createServer((req, res) => {
@@ -9,11 +11,20 @@ const server = http.createServer((req, res) => {
 	let method = req.method.toLowerCase();
 	let queryStringObject = parsedUrl.query;
 	let headers = req.headers;
-	res.end("Hello World\n");
-	console.log("Request received from: " + trimmedPath);
-	console.log("HTTP Method: " + method);
-	console.log("Query String: ", queryStringObject);
-	console.log("Header: ", headers);
+	let decoder = new StringDecoder('utf-8');
+	let buffer = '';
+	req.on('data', (data) => {
+		buffer += decoder.write(data);
+	});
+	req.on('end', () => {
+		buffer += decoder.end();
+		res.end("Hello World\n");
+		console.log("Request received from: " + trimmedPath);
+		console.log("HTTP Method: " + method);
+		console.log("Query String: ", queryStringObject);
+		console.log("Header: ", headers);
+		console.log("Payload", buffer);
+	});
 });
 
 server.listen(port);
